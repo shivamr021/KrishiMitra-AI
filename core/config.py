d46666d64f59083989802c01e4e95cabb137a7ec
Shivam Rathod <146384849+shivamr021@users.noreply.github.com>
2026-01-11 11:06:33 +0530

    Fix indentation for TWILIO_AUTH_TOKEN

--- core/config.py
+++ core/config.py
@@ -1,18 +1,15 @@
-# core/config.py
 import os
 from pydantic_settings import BaseSettings
 from pathlib import Path
 
-# Define the base directory of your project
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
     WEATHER_API_KEY: str
     GEMINI_API_KEY: str
     TWILIO_ACCOUNT_SID: str
-    TWILIO_AUTH_TOKEN: str  
+    TWILIO_AUTH_TOKEN: str
 
-    # Paths
     MODEL_PATH: str = os.path.join(BASE_DIR, 'model', 'saved_model', 'krishi_multicrop_model.keras')
     CLASS_NAMES_PATH: str = os.path.join(BASE_DIR, 'model', 'class_names.json')
     INDIAN_CITIES_PATH: str = os.path.join(BASE_DIR, 'static', 'data.json')
