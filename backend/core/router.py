f2f056779d1bb90b4a63f1adc3dd73e66be8ab2c
Shivam Rathod <shivamrathod145@gmail.com>
2026-05-24 20:39:25 +0530

    Added assets and evaluation tests

--- backend/core/router.py
+++
@@ -1,9 +0,0 @@
-from agents import market_price_agent, weather_agent
-
-def get_market_price(query: str, location: str = 'Khargone') -> str:
-    """Tool to get market price."""
-    return market_price_agent.get_market_price(query, location)
-
-def get_weather_forecast(location: str) -> str:
-    """Tool to get weather forecast."""
-    return weather_agent.get_weather_forecast(location)
